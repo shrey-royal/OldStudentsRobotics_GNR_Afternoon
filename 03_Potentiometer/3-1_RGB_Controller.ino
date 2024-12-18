bdd186d3a93e34ab75bf46bb2e3ea1215d7f2055
Shrey K <srkkadia@gmail.com>
2024-12-18 09:51:36 +0530

    RGB Controller

---
+++ 03_Potentiometer/3-1_RGB_Controller.ino
@@ -0,0 +1,19 @@
+int r=9, g=10, b=11;
+int p1=A0, p2=A1, p3=A2;
+
+void setup()
+{
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(p1, INPUT);
+  pinMode(p2, INPUT);
+  pinMode(p3, INPUT);
+}
+
+void loop()
+{
+  analogWrite(r, analogRead(p1)/4);
+  analogWrite(g, analogRead(p2)/4);
+  analogWrite(b, analogRead(p3)/4);
+}
